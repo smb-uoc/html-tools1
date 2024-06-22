851de1444e04182c77ed72ab552f65165d2fd427
smb-uoc <154095408+smb-uoc@users.noreply.github.com>
2024-06-22 20:06:09 +0000

    change project to single folder for PECs

--- Modules/M2/teste 1/index.js
+++
@@ -1,7 +0,0 @@
-import logSymbols from "log-symbols";
-
-console.log('¡Hola, mundo desde mi primer archivo JavaScript!');
-console.log(logSymbols.info, 'Funciona');
-console.log(logSymbols.success, 'Funciona');
-console.log(logSymbols.error, 'Funciona');
-console.log(logSymbols.warning, 'Funciona');
