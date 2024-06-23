9d68d30f942a9319bea3f4ed67c5d75084e486c3
smb-uoc <smartinezbae@uoc.edu>
2024-06-23 22:35:33 +0200

    add PEC3

--- src/js/fanzone.js
+++ src/js/fanzone.js
@@ -9,24 +9,26 @@ export function addComments(data, hasEffect) {
         if(hasEffect) {
             comentarioDiv.classList.add("typing-animation");
         }
-
         const nombreParrafo = document.createElement("p");
+
         nombreParrafo.classList.add("nombre");
         nombreParrafo.textContent = comentario.nombre;
         comentarioDiv.appendChild(nombreParrafo);
-
         const comentarioParrafo = document.createElement("p");
+
         comentarioParrafo.classList.add("comentario-texto");
         comentarioParrafo.textContent = comentario.comentario;
         comentarioDiv.appendChild(comentarioParrafo);
-
         const horaParrafo = document.createElement("p");
+
         horaParrafo.classList.add("datetime");
         horaParrafo.textContent = new Date(comentario.datetime).toDateString();
         comentarioDiv.appendChild(horaParrafo);
-
         let primerElemento = comentariosDiv.firstChild;
-        comentariosDiv.insertBefore(comentarioDiv, primerElemento);
+
+        const comentarioLi = document.createElement("li");
+        comentarioLi.appendChild(comentarioDiv);
+        comentariosDiv.insertBefore(comentarioLi, primerElemento);
     });
 }
 
